e7542fe9d0c8b7b60539c599dae4e93d9416e01b
mahendra_dwivedi <dwivedimahendra999912@gmail.com>
2024-02-02 20:28:09 +0530

    initail commit

---
+++ src/Components/Card.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import {FcLike , FcLikePlaceholder} from "react-icons/fc"
+import { toast } from "react-toastify";
+
+const Card = (props) => {
+
+    let course = props.course;
+    let likedCourses = props.likedCourses;
+    let setLikedCourses = props.setLikedCourses;
+
+    function clickhandler() {
+        // logic
+        if(likedCourses.includes(course.id)) {
+            // phele se like hoa hai
+            setLikedCourses((prev) => prev.filter((cid) => (cid !== course.id)));
+            toast.warning("Like Removed");
+        }
+        else{
+            // phele se like nhi hai
+            // insert karna h ye course liked courses me
+            if (likedCourses.length === 0 ){
+                setLikedCourses([course.id]);
+
+            }
+            else {
+                // non-empty pehle se
+                setLikedCourses((prev) => [...prev, course.id]);
+            }
+            toast.success("Liked Successfully")
+        }
+    }
+    return ( 
+        <div className='bg-bgDark bg-opacity-80 w-[300px] rounded-md overflow-hidden'>
+
+            <div className='relative '>
+                <img src={course.image.url} ></img>
+
+                <div className='rounded-full w-[40px] h-[40px] bg-white absolute right-2 bottom-[-12px] grid place-items-center'>
+                <button onClick={clickhandler} >
+                    {
+                        likedCourses.includes(course.id) ? 
+                        (<FcLike fontSize="1.75rem"></FcLike> ) :
+                        (<FcLikePlaceholder fontsize="1.75rem" /> ) 
+                    }
+                </button>    
+            </div>
+            </div>
+            
+            <div className='p-4'>
+                <p className='text-white text-lg font-semibold leading-6'>{course.title}</p>
+                <p className='mt-2 text-white' >
+                    {
+                        course.description.length > 100 ? 
+                        (props.course.description.substring(0, 100) + "...") : 
+                        (props.course.description)
+                    }
+                </p>
+            </div>
+
+        </div>
+    )
+}
+
+export default Card;
